b02b834541729c71ab28317d02778533d1796019
leonel2500 <strokehead@outlook.es>
2025-03-20 12:12:01 -0400

    Cards agregadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="mx-auto">Grupo Yalasoft</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Card style={{ minWidth: '18rem', margin: '20px' }}>
+              <Card.Img variant="top" src="..." />
+              <Card.Body>
+                <Card.Title>Example Card</Card.Title>
+                <Card.Text>This is an example React card</Card.Text>
+                <Button variant="primary">Example Button</Button>
+              </Card.Body>
+            </Card>
+
+          </Col>
+
+          <Col>
+
+            <Card style={{ minWidth: '18rem', margin: '20px' }}>
+              <Card.Img variant="top" src="..." />
+              <Card.Body>
+                <Card.Title>Example Card</Card.Title>
+                <Card.Text>This is an example React card</Card.Text>
+                <Button variant="primary">Example Button</Button>
+              </Card.Body>
+            </Card>
+
+          </Col>
+
+          <Col>
+
+            <Card style={{ minWidth: '18rem', margin: '20px' }}>
+              <Card.Img variant="top" src="..." />
+              <Card.Body>
+                <Card.Title>Example Card</Card.Title>
+                <Card.Text>This is an example React card</Card.Text>
+                <Button variant="primary">Example Button</Button>
+              </Card.Body>
+            </Card>
+
+          </Col>
+        </Row>
+
+      </Container>
+
     </>
   )
 }
