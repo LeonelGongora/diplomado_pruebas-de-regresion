3af7fb98d8622cdf9509dff5424740e3e0a673da
Leonel_Gongora <strokehead@outlook.es>
2025-03-20 21:06:07 -0400

    Se agrego informacion de los otros productos

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Modal from 'react-bootstrap/Modal';
 //import Bateria from '../public/Bateria_Pearl.jpg';
 function App() {
   const [modalCarrito, setModalCarrito] = useState(false);
-  const [modalDetalles, setModalDetalles] = useState(false);
 
   return (
     <>
@@ -22,30 +21,54 @@ function App() {
 
       <Container className="mt-5">
         <Row>
-          <Col xs={4}>
+          <Col>
             <Card style={{ minWidth: '18rem', margin: '20px' }}>
-              <Card.Img variant="top" src= "/Bateria_Pearl.jpg" alt="image"/>
+              <Card.Img variant="top" src= "/Bateria_Pearl.jpg" alt="image" style={{ width: '100%', height: '250px', objectFit: 'cover' }} />
               <Card.Body>
-                <Card.Title>Nombre</Card.Title>
-                <Card.Text>Descripcion</Card.Text>
+                <Card.Title>Pearl bateria acustica – Export EXX725SP/C</Card.Title>
+                <Card.Text>Acabado lacado, Capas de álamo y caoba, Configuración 10-12-14-16-22.</Card.Text>
                 <Row>
-                  
                   <Col>
                     <Button variant="warning" onClick={() => setModalCarrito(true)}>Añadir al carrito</Button>
                   </Col>
-
-                  <Col>
-                    <Button variant="warning" onClick={() => setModalDetalles(true)}>Mas detalles</Button>
-                  </Col>
-
                 </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+, 
+          <Col >
+            <Card style={{ minWidth: '18rem', margin: '20px' }}>
+              <Card.Img variant="top" src= "/Fundacion.jpg" alt="image" style={{ width: '100%', height: '250px', objectFit: 'cover' }} />
+              <Card.Body>
+                <Card.Title>La Trilogia de la Fundación</Card.Title>
+                <Card.Text>La Trilogía de la Fundación está considerada la mejor serie de la ciencia ficción universal. Fue galardonada en 1966 con un premio Hugon</Card.Text>
+                <Row>
+                  <Col>
+                    <Button variant="warning" onClick={() => setModalCarrito(true)}>Añadir al carrito</Button>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
 
+          <Col>
+            <Card style={{ minWidth: '18rem', margin: '20px' }}>
+              <Card.Img variant="top" src= "/Led_Zeppelin.jpg" alt="image" style={{ width: '100%', height: '250px', objectFit: 'cover' }} />
+              <Card.Body>
+                <Card.Title>Led Zeppelin IV</Card.Title>
+                <Card.Text>Es el cuarto álbum de estudio de la banda de rock inglesa Led Zeppelin, lanzado el 8 de noviembre de 1971 </Card.Text>
+                <Row>
+                  <Col>
+                    <Button variant="warning" onClick={() => setModalCarrito(true)}>Añadir al carrito</Button>
+                  </Col>
+                </Row>
               </Card.Body>
             </Card>
           </Col>
         </Row>
 
       </Container>
+      
 
       <Modal
         show={modalCarrito}
@@ -69,27 +92,6 @@ function App() {
         </Modal.Footer>
       </Modal>
 
-      <Modal
-        show={modalDetalles}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        onHide={() => setModalDetalles(false)}
-        centered
-      >
-        <Modal.Header closeButton className="bg-danger text-white" style={{ border: 'none' }}>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Error!
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="bg-danger text-white" style={{ border: 'none' }}>
-          <div className='container-fluid'>
-            <div className='fw-bold' style={{ fontSize: '24px' }}>No existe el producto seleccionado.</div>
-          </div>
-
-        </Modal.Body>
-        <Modal.Footer className="bg-danger text-white" style={{ border: 'none' }}>
-        </Modal.Footer>
-      </Modal>
     </>
   )
 }
